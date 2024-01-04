3b6d2d37e985a57d41063739f0565750b77d5243
Kritagya Dahal <kritagyadahal345@gmail.com>
2024-01-04 16:55:24 +0000

    commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+# configure aws provider
+provider "aws" {
+  region = "eu-west-2"
+  profile = "terraform-user"
+}
+
+# stores the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket321"
+    key    = "terraform.tfstate.dev"
+    region = "eu-west-1"
+    profile = "terraform-user"
+  }
+}
+
